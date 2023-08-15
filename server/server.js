2d6746b1765ae426ebb4dc4c9115e2a900660043
cesarsiguencia <cesar.siguencia@gmail.com>
2023-08-15 11:20:59 -0400

    uploaded without node_modules

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const mongoose = require('mongoose')
+
+const app = express.app
+
+const PORT = process.env.PORT || 4100;
+
+const routes = require('./routes')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.use(routes)
+
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/i-can-fix-it', {
+    userNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true)
+
+app.listen(PORT, ()=> console.log(`iCanFixIt listening on ${PORT}`))
