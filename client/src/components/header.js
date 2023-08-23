f66a9cd15845ff55e78d6cc538233e00b6c5c517
cesarsiguencia <cesar.siguencia@gmail.com>
2023-08-23 13:43:03 -0400

    added my personal functions for deteching what page the user is in so that header will highlight current selected page in navbar

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 // import { Nav} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
@@ -9,7 +9,7 @@ const Header = () => {
 
     const [titleClicked, setTitleClicked] = useState()
     const [navClicked, setNavClicked] = useState()
-  
+
     // if(portfolioClicked === true){
     //   setTitleClicked(false)
     //   setNavClicked(true)
@@ -18,99 +18,113 @@ const Header = () => {
 
     const pages = [
         {
-          name: "About",
-          url: "about"
+            name: "About",
+            url: "about"
         },
         {
-          name: 'Gallery',
-          url: "gallery"
+            name: 'Gallery',
+            url: "gallery"
         },
         {
-          name: "Review Me",
-          url: "review"
+            name: "Review Me",
+            url: "review"
         },
         {
-          name: "Request Service",
-          url: "form",
-          image: toolbar
+            name: "Request Service",
+            url: "form",
+            image: toolbar
         }
-      ]
-    
-    //   let subUrlObjectAtLoad
-    //   const urlsArray = window.location.href.split('/')
-    //   const urlOfSubPage = urlsArray[urlsArray.length - 1] 
-      
-    //   if(urlOfSubPage!== 'my-react-portfolio'){
-    //     subUrlObjectAtLoad = {
-    //       urlSub: urlOfSubPage,
-    //       urlSubLoaded: true
-    //     }
-    //   } else {
-    //     subUrlObjectAtLoad = {
-    //       urlSub: "not a urlSub",
-    //       urlSubLoaded: false
-    //     }
-    //   }
-    
-    //   let currentIndex
-    //   const loadedPage = subUrlObjectAtLoad.urlSub
-    
-    //   if(loadedPage){
-    //     pages.forEach((page) => {
-    //       if(page.url === loadedPage){
-    //         currentIndex = pages.indexOf(page)
-    //       }
-    //     })
-    //   }
-    
-    //   const [selectedPage, setSelectedPage] = useState(pages[currentIndex])
+    ]
+
+    let subUrlObjectAtLoad
+    const urlsArray = window.location.href.split('/')
+    const urlOfSubPage = urlsArray[urlsArray.length - 1]
+    console.log(urlOfSubPage)
+
+    if (urlOfSubPage !== 'icanfixit') {
+        subUrlObjectAtLoad = {
+            urlSub: urlOfSubPage,
+            urlSubLoaded: true
+        }
+    } else {
+        subUrlObjectAtLoad = {
+            urlSub: "not a urlSub",
+            urlSubLoaded: false
+        }
+    }
+
+    let currentIndex
+    const loadedPage = subUrlObjectAtLoad.urlSub
+
+    if (loadedPage) {
+        pages.forEach((page) => {
+            if (page.url === loadedPage) {
+                currentIndex = pages.indexOf(page)
+            }
+        })
+    }
+
+    const [selectedPage, setSelectedPage] = useState(pages[currentIndex])
 
     return (
         <header className='App-header'>
             <Navbar expand="lg">
                 <Container className='header-components'>
                     <Navbar.Brand>
-                        <Link  to='/icanfixit' className='nav-links-font' onClick={()=>{
+                        <Link to='/icanfixit' className='nav-links-font' onClick={() => {
                             setTitleClicked(true)
                             setNavClicked(false)
                         }}>
                             <div class='text-align-left'>
-                            <p className='nav-links-font header-title'>iCanFixIt</p>
-                            <small className='header-desc'>Repair Your Device With Me!</small>
+                                <p className='nav-links-font header-title'>iCanFixIt</p>
+                                <small className='header-desc'>Repair Your Device With Me!</small>
                             </div>
-                            
+
                         </Link >
                     </Navbar.Brand>
                     <Navbar.Toggle className='toggle' aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse  id="basic-navbar-nav" >
-                        <Nav className="me-auto" 
-                        onClick={()=>{
-                            setNavClicked(true)
-                            setTitleClicked(false)
+                    <Navbar.Collapse id="basic-navbar-nav" >
+                        <Nav className="me-auto"
+                            onClick={() => {
+                                setNavClicked(true)
+                                setTitleClicked(false)
                             }}
                         >
-                            {pages.map((page)=>{
-                                if(!page.image){
-                                    return(
-                                        <Nav.Link>
-                                        <Link className='nav-links-font links' to={`/icanfixit/${page.url}`}>{page.name}
-                                        </Link>
-                                    </Nav.Link>
+                            {pages.map((page, i) => {
+                                if (!page.image) {
+                                    return (
+                                        <Nav.Link 
+
+                                        onClick={() => {
+                                            setSelectedPage(page)
+                                          }}
+                                            
+                                        key={i}>
+                                            <Link className={`nav-links-font links     ${!selectedPage ? (selectedPage == "") : (selectedPage.name === page.name && !titleClicked && navClicked && 'links-selected')}`} to={`/icanfixit/${page.url}`}>{page.name}
+                                            </Link>
+                                        </Nav.Link>
                                     )
                                 } else {
-                                    return(
-                                        <Nav.Link className='featured-button'>
-                                        <Link className='nav-links-font links' to={`/icanfixit/${page.url}`}>{page.name}
-                                        </Link>
-                                        <img className='toolbar' src={page.image}>
-                                        </img>
-                                    </Nav.Link>
-        
+                                    return (
+                                        <Nav.Link 
+                                        onClick={() => {
+                                            setSelectedPage(page)
+                                          }}
+                                        
+                                        key={i} className='featured-button'>
+                                            <Link className='nav-links-font links' 
+                                            
+                                            to={`/icanfixit/${page.url}`}>{page.name}
+                                            </Link>
+                                            <img className='toolbar' src={page.image}>
+                                            </img>
+                                        </Nav.Link>
+
                                     )
                                 }
-                         
+
                             })}
-       
+
 
 
                         </Nav>
@@ -118,7 +132,7 @@ const Header = () => {
                 </Container>
             </Navbar>
 
-        
+
 
         </header>
 
