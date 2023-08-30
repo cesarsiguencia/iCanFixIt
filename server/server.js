c035a8b2aab7e2d7ec915ed441bf8a79689d1d53
cesarsiguencia <cesar.siguencia@gmail.com>
2023-08-30 17:28:39 -0400

    moved public folder to server side

--- server/server.js
+++ server/server.js
@@ -10,22 +10,22 @@ const PORT = process.env.PORT || 4101;
 // const routes = require('./routes')
 
 // app.use(cors())
-app.get('/', (req, res) => {
-    res.send({message: 'Working server'})
-})
+// app.get('/', (req, res) => {
+//     res.send({message: 'Working server'})
+// })
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 // app.use(express.static('public'))
 app.use(require('./routes'))
 
-// //This middleware will tell the application to use the built react-app
-// app.use(express.static(path.join(__dirname, "public")));
+//This middleware will tell the application to use the built react-app
+app.use(express.static(path.join(__dirname, "public")));
 
-// //Put this after all middleware. Otherwise, Heroku will give you 304 page
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
+//Put this after all middleware. Otherwise, Heroku will give you 304 page
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 
 mongoose.connect(process.env.MONGO_URI || process.env.LOCALHOST, {
