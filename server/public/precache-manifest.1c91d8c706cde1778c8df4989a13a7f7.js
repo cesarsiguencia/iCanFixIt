0aab7623e1a867674ee1ddadf2517c7670f8e827
cesarsiguencia <cesar.siguencia@gmail.com>
2023-08-30 19:38:13 -0400

    new build with 404.html code, og, and icon

---
+++ server/public/precache-manifest.1c91d8c706cde1778c8df4989a13a7f7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf27cb5b71ffcb762e54",
+    "url": "/static/css/main.d8a28927.chunk.css"
+  },
+  {
+    "revision": "cf27cb5b71ffcb762e54",
+    "url": "/static/js/main.debe8028.chunk.js"
+  },
+  {
+    "revision": "753201d7370095a75750",
+    "url": "/static/js/runtime~main.03d47f1d.js"
+  },
+  {
+    "revision": "a5288b6b15584beb87cf",
+    "url": "/static/css/2.8e766de6.chunk.css"
+  },
+  {
+    "revision": "a5288b6b15584beb87cf",
+    "url": "/static/js/2.811e81a5.chunk.js"
+  },
+  {
+    "revision": "23c09f75324934ab93d3",
+    "url": "/static/js/3.e660b796.chunk.js"
+  },
+  {
+    "revision": "ff1644077f28647058b7b90c817bf095",
+    "url": "/static/media/success.ff164407.gif"
+  },
+  {
+    "revision": "a21746f5374859d98f7528e1857e3532",
+    "url": "/static/media/ZKZg.a21746f5.gif"
+  },
+  {
+    "revision": "b1aaa749691eaa63e36fb039829e470b",
+    "url": "/static/media/hero.b1aaa749.png"
+  },
+  {
+    "revision": "61b4b00553acaee0f2edf2b921b31ba0",
+    "url": "/static/media/dog-running.61b4b005.gif"
+  },
+  {
+    "revision": "9dcf3926a4891fd97724fe80e1b0cd08",
+    "url": "/static/media/complete-form.9dcf3926.gif"
+  },
+  {
+    "revision": "07990de3a791c7330131934bc546ad25",
+    "url": "/static/media/email-received.07990de3.gif"
+  },
+  {
+    "revision": "b04e20c2c695ad04da6a41480b085235",
+    "url": "/static/media/shipping.b04e20c2.gif"
+  },
+  {
+    "revision": "1a96a149bb8160dd71093b71deb22b49",
+    "url": "/static/media/portrait.1a96a149.png"
+  },
+  {
+    "revision": "588cf279ea4c85888bf7a5712aaebb47",
+    "url": "/static/media/mobile.588cf279.gif"
+  },
+  {
+    "revision": "806e82b77a3775e97484ce87d22b0555",
+    "url": "/static/media/wrench-svgrepo-com.806e82b7.svg"
+  },
+  {
+    "revision": "07883e93734b98cae0f7b9c55d287250",
+    "url": "/static/media/github.07883e93.svg"
+  },
+  {
+    "revision": "df721c2187edc80e4b51abf012cfefc5",
+    "url": "/static/media/linkedin.df721c21.png"
+  },
+  {
+    "revision": "3ee81c6947903fc2f4cd91bd0e67a931",
+    "url": "/index.html"
+  }
+];
