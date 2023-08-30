f0b3ff95fb5d79c155b6467afb28cbc3afcfa2a9
cesarsiguencia <cesar.siguencia@gmail.com>
2023-08-30 15:08:51 -0400

    removing conflicting packages from root

--- server/server.js
+++ server/server.js
@@ -31,12 +31,4 @@ mongoose.connect(process.env.MONGO_URI || process.env.CONNECTION_URL, {
 
 mongoose.set('debug', true)
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-
 app.listen(PORT, ()=> console.log(`iCanFixIt listening on ${PORT}`))
