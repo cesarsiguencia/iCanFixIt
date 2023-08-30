d67e4fce1e51c0bb417c434b9ab402c90d40dd0e
cesarsiguencia <cesar.siguencia@gmail.com>
2023-08-30 15:35:08 -0400

    running with older version of node

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 // const cors = require('cors')
-const path = require('path')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const app = express()
@@ -8,7 +7,6 @@ const app = express()
 const PORT = process.env.PORT || 4101;
 
 // const routes = require('./routes')
-
 
 // app.use(cors())
 app.get('/', (req, res) => {
@@ -19,7 +17,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 // app.use(express.static('public'))
 app.use(require('./routes'))
-
 
 
 mongoose.connect(process.env.MONGO_URI || process.env.CONNECTION_URL, {
