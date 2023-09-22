d91ffab89d1756ffac82a7988e3b5b79a1bb0ed2
cesarsiguencia <cesar.siguencia@gmail.com>
2023-09-22 01:06:45 -0400

    new build moved to static server public folder

---
+++ server/public/precache-manifest.93360556ab9f9853bcb4793b6ea76f2f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3a2a73f24b3b507a0d1",
+    "url": "/iCanFixIt/static/css/main.fa4c0649.chunk.css"
+  },
+  {
+    "revision": "c3a2a73f24b3b507a0d1",
+    "url": "/iCanFixIt/static/js/main.dbe73ff1.chunk.js"
+  },
+  {
+    "revision": "0585baaeccd312a6c336",
+    "url": "/iCanFixIt/static/js/runtime~main.6d7bc9f0.js"
+  },
+  {
+    "revision": "7fc502d713ba252d650b",
+    "url": "/iCanFixIt/static/css/2.8e766de6.chunk.css"
+  },
+  {
+    "revision": "7fc502d713ba252d650b",
+    "url": "/iCanFixIt/static/js/2.d26ace70.chunk.js"
+  },
+  {
+    "revision": "50ee3c8b5b065e8c8cba",
+    "url": "/iCanFixIt/static/js/3.26d8ba2d.chunk.js"
+  },
+  {
+    "revision": "66ef4d755515fafe0f7d3821af66491c",
+    "url": "/iCanFixIt/static/media/user.66ef4d75.avif"
+  },
+  {
+    "revision": "ba43dc20e13107e5177fa671711971e1",
+    "url": "/iCanFixIt/static/media/credentials.ba43dc20.avif"
+  },
+  {
+    "revision": "ff1644077f28647058b7b90c817bf095",
+    "url": "/iCanFixIt/static/media/success.ff164407.gif"
+  },
+  {
+    "revision": "a21746f5374859d98f7528e1857e3532",
+    "url": "/iCanFixIt/static/media/ZKZg.a21746f5.gif"
+  },
+  {
+    "revision": "b1aaa749691eaa63e36fb039829e470b",
+    "url": "/iCanFixIt/static/media/hero.b1aaa749.png"
+  },
+  {
+    "revision": "61b4b00553acaee0f2edf2b921b31ba0",
+    "url": "/iCanFixIt/static/media/dog-running.61b4b005.gif"
+  },
+  {
+    "revision": "9dcf3926a4891fd97724fe80e1b0cd08",
+    "url": "/iCanFixIt/static/media/complete-form.9dcf3926.gif"
+  },
+  {
+    "revision": "07990de3a791c7330131934bc546ad25",
+    "url": "/iCanFixIt/static/media/email-received.07990de3.gif"
+  },
+  {
+    "revision": "b04e20c2c695ad04da6a41480b085235",
+    "url": "/iCanFixIt/static/media/shipping.b04e20c2.gif"
+  },
+  {
+    "revision": "1a96a149bb8160dd71093b71deb22b49",
+    "url": "/iCanFixIt/static/media/portrait.1a96a149.png"
+  },
+  {
+    "revision": "588cf279ea4c85888bf7a5712aaebb47",
+    "url": "/iCanFixIt/static/media/mobile.588cf279.gif"
+  },
+  {
+    "revision": "806e82b77a3775e97484ce87d22b0555",
+    "url": "/iCanFixIt/static/media/wrench-svgrepo-com.806e82b7.svg"
+  },
+  {
+    "revision": "07883e93734b98cae0f7b9c55d287250",
+    "url": "/iCanFixIt/static/media/github.07883e93.svg"
+  },
+  {
+    "revision": "df721c2187edc80e4b51abf012cfefc5",
+    "url": "/iCanFixIt/static/media/linkedin.df721c21.png"
+  },
+  {
+    "revision": "a090e598f29d665eadf356edd33504c1",
+    "url": "/iCanFixIt/index.html"
+  }
+];
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b6fb72909304df4f6b2901a019e8e739.js"
+  "/iCanFixIt/precache-manifest.93360556ab9f9853bcb4793b6ea76f2f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/iCanFixIt/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
